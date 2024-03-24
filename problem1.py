a06bf9d79e4dfcfbf982163730fd7c296a3f536e
Nafiz_ <164038292+nafizgit@users.noreply.github.com>
2024-03-24 21:47:36 +0600

    problem1.py

---
+++ problem1.py
@@ -0,0 +1,9 @@
+#find rectangle or Square
+
+a = int(input("Leanth Of Rectangle:"))
+b = int(input("Breadth  Of Rectangle"))
+
+if a==b:
+    print("This Is Square ")
+else:
+    print("This is Rectangle")
